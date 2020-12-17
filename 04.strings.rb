7b3f4716d83f17a7f731ae166a7ad9d1fdc090e1
gowthamaraj <gowthamaraj360@gmail.com>
2020-12-16 17:51:42 -0800

    add strings

---
+++ 04.strings.rb
@@ -0,0 +1,19 @@
+puts "hi"
+puts "hi\""
+puts 'hi"'
+
+phrase = "Dreams do come true"
+
+#upcase, downcase
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.strip()
+
+
+puts phrase.length()
+puts phrase.include? "dreams"
+
+
+puts phrase[0]
+puts phrase[0,3]
+puts phrase.index("c")
