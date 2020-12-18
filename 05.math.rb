44e485bfe1e90115cbeb0161c21784a216652514
gowthamaraj <gowthamaraj360@gmail.com>
2020-12-17 18:05:17 -0800

    add math

---
+++ 05.math.rb
@@ -0,0 +1,24 @@
+puts 5
+puts -5.8
+puts 0.1+0.2
+puts 5/9
+puts 2**3
+puts 2*3
+puts 10%3
+
+#var
+num = 20
+puts ("i love the number "+num.to_s)
+
+num = -10.11
+puts num.abs()
+puts num.round()
+puts num.floor()
+puts num.ceil()
+
+
+puts Math.sqrt(1)
+puts Math.log(10)
+
+
+
