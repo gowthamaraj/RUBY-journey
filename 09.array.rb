7d94c2608112a15abb4f3191104045ce9a6e2268
gowthamaraj <gowthamaraj360@gmail.com>
2020-12-22 18:25:16 -0800

    add arrays

---
+++ 09.array.rb
@@ -0,0 +1,13 @@
+friends = Array['kevin','karen','oscar']
+
+puts friends[1]
+puts friends[0,2]
+
+food = Array.new
+food[1] = "chocos"
+
+puts food
+
+puts food.include?"chocos"
+puts friends.reverse()
+puts friends.sort()
