6acb2cd6c33340a9a51ddd660e7d80495617a91f
gowthamaraj <gowthamaraj360@gmail.com>
2020-12-28 19:54:09 -0800

    add case

---
+++ 15.case.rb
@@ -0,0 +1,16 @@
+def abb(word)
+case word
+when "mon"
+	puts "Monday"
+when "tue"
+	puts "Tuesday"
+when "wed"
+	puts "Tuesday"
+when "thur"
+	puts "Thursday"
+when "fri"
+	puts "Friday"
+end
+end
+
+abb("mon")
